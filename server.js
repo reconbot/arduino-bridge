2384a61ede3283c8a66bcbf3d71936416694cf98
Francis Gulotta <wizard@roborooter.com>
2012-11-11 19:01:38 -0500

    redirect

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ var app = express();
 var server = http.createServer(app);
 var io = sio.listen(server);
 
+var url = 'http://coalition-for-the-li.nko3.jit.su/success';
 
 app.get('/message',function(req,res){
   if(typeof req.query.text == 'string' && req.query.text){
     io.sockets.emit('text', req.query.text);
-    res.end('OK');
+    res.redirect(url);
   }else{
     res.end('notok');
   }
